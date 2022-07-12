c01f5b905fdc58a642efb477d615559a69c256ef
Mehrojiddin <mehrojiarkom@gmail.com>
2022-07-12 23:55:51 +0500

    # Task 5 Life

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ We have number and we need find ertain percentage from number:
 Find 10% from 560
 # Task 4 Number from procen
 We have x and y. If x is 100%. What percentage of y is of x?:
+# Task 5 Life
+We need enter our years and system need show us how many years, months, weaks, days, hours, minuts, secunds we live:
+
--- js/main.js
+++ js/main.js
@@ -15,6 +15,18 @@
 // console.log(`${x*y/100}`);
 
 // Task 4
-let x = +prompt("Enter first number ");
-let y = +prompt("Enter second number: ");
-console.log(`${x*100/y}`);
+// let x = +prompt("Enter first number ");
+// let y = +prompt("Enter second number: ");
+// console.log(`${x*100/y}`);4
+
+// Task 5
+let x = +prompt ("Chanka umr diday?")
+console.log(`${res=x/100}century`);
+console.log(`${res=x}years`);
+console.log(`${res=x*12}months`);
+console.log(`${res=x*12*4.5}weaks`);
+console.log(`${res=x*12*4.5*7}days`);
+console.log(`${res=x*12*4.5*7*24}hours`);
+console.log(`${res=x*12*4.5*7*24*60}minuts`);
+console.log(`${res=x*12*4.5*7*24*60*60}secunds`);
+
