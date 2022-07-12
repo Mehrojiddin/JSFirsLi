3cb73bdf0345f0718462494ed46603db82037ee4
Mehrojiddin <mehrojiarkom@gmail.com>
2022-07-12 23:38:27 +0500

    Task 2 The equation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Task 1 perimetr
 We need find perimetr: p=2(x+y)
+
+# Task 2 The equation
+We need do this equation: y=x*2+x*3+6
--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 // Task 1
-let x = +prompt("Enter your number: ")
-let y = +prompt("Enter your number: ")
-let res = 2*(x+y);
-console.log(`${res}`);
+// let x = +prompt("Enter your number: ")
+// let y = +prompt("Enter your number: ")
+// let res = 2*(x+y);
+// console.log(`${res}`);
+
+
+// Task 2
+let x = +prompt("Enter value: ");
+console.log(`${x**2+x**3+6}`);
