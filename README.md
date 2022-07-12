e1cbdfcab6b72994136d7f20879e1ccefec0ed04
Mehrojiddin <mehrojiarkom@gmail.com>
2022-07-12 23:32:19 +0500

    FLes

---
+++ README.md
@@ -0,0 +1,2 @@
+# Task 1 perimetr
+We need find perimetr: p=2(x+y)
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Task 1
+let x = +prompt("Enter your number: ")
+let y = +prompt("Enter your number: ")
+let res = 2*(x+y);
+console.log(`${res}`);
